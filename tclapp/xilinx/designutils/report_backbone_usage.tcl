4269f1aba97f52b8f1e6874412846230951347dc
fmuelle <fmuelle@xilinx.com>
2014-04-17 00:42:21 -0600

    updated report_backbone_usage proc

--- tclapp/xilinx/designutils/report_backbone_usage.tcl
+++ tclapp/xilinx/designutils/report_backbone_usage.tcl
@@ -67,37 +67,57 @@ proc ::tclapp::xilinx::designutils::report_backbone_usage::report_backbone_usage
 
 	set i 1
 	set overallStatus {}
+	set CDR_backbone_IBUF_I_overall 0
 	foreach net $nets {
+		set CDR_backbone_IBUF_I 0
+		# if CLOCK_DEDICATED_ROUTE=BACKBONE constraint is applied to input of IBUF*
+		if {[string match IBUF* [get_lib_cells -of [get_cells -of [get_pins -leaf -of $net -filter DIRECTION==IN]]]]} {
+			set CDR_backbone_IBUF_I 1
+			set CDR_backbone_IBUF_I_overall 1
+			set net [get_nets -quiet -of [get_pins -quiet -of [get_cells -quiet -of [get_pins -leaf -of $net -filter DIRECTION==IN] -filter LIB_CELL=~IBUF*] -filter REF_PIN_NAME=~O]]
+		}
 		set sourceCell [get_lib_cells -of [get_cells -of [get_pins -leaf -of $net -filter DIRECTION==OUT]]]
 		set sourceClockRegion [lsort -unique -decreasing [get_clock_regions -of [get_cells -of [get_pins -leaf -of $net -filter DIRECTION==OUT]]]]
 		set destCell [get_lib_cells -of [get_cells -of [get_pins -leaf -of $net -filter DIRECTION==IN]]]
 		set destClockRegion [lsort -unique -decreasing [get_clock_regions -of [get_cells -of [get_pins -leaf -of $net -filter DIRECTION==IN]]]]
 		#need to check for CDR=BACKBONE constraint on net of IBUFDS I pin of $net and $net 
-		set IBUFDS_Ipin_net [get_nets -quiet -of [get_pins -quiet -of [get_cells -quiet -of [get_pins -leaf -of $net -filter DIRECTION==OUT] -filter LIB_CELL=~IBUFDS*] -filter REF_PIN_NAME=~I]] 
+		set IBUFDS_Ipin_net [get_nets -quiet -of [get_pins -quiet -of [get_cells -quiet -of [get_pins -leaf -of $net -filter DIRECTION==OUT] -filter LIB_CELL=~IBUF*] -filter REF_PIN_NAME=~I]] 
 		if {$IBUFDS_Ipin_net ne ""} {
 			set constraint [lsort -unique [concat [get_property CLOCK_DEDICATED_ROUTE $net] [get_property CLOCK_DEDICATED_ROUTE $IBUFDS_Ipin_net]]]
+			if {[get_property CLOCK_DEDICATED_ROUTE $IBUFDS_Ipin_net]!=""} {
+				set CDR_backbone_IBUF_I 1
+				set CDR_backbone_IBUF_I_overall 1
+			}
 		} else {
 			set constraint [get_property CLOCK_DEDICATED_ROUTE $net] 
+		}
+		if {$CDR_backbone_IBUF_I} {
+			set constraint [concat $constraint " *"]
 		}
 		set BBused [lsort -unique [get_clock_regions -quiet -of  [get_sites -quiet -of [get_tiles -quiet -of [get_nodes -quiet -of $net -filter NAME=~*/${BBnodePattern}]]]]]
 		set BBnode [file tail [lindex [get_nodes -quiet -of $net -filter NAME=~*/${BBnodePattern}] 0]] 
 		if {[llength $BBused] == 0} {
 			set status "Fabric"
-		} elseif {[llength $BBused] == [llength [get_backbone_clk_regions $sourceClockRegion $destClockRegion]]} { 
+		} elseif {[llength $BBused] == [llength [get_BB_clk_regions $sourceClockRegion $destClockRegion]]} { 
 			set status "All Backbone"
 		} else {
 			set status "Partial Backbone"
 		}
 		if {$table} {
-			$net_table addrow [list $i $net $BBnode $sourceCell $destCell $sourceClockRegion $destClockRegion $BBused [get_backbone_clk_regions $sourceClockRegion $destClockRegion] $constraint $status]
+			$net_table addrow [list $i $net $BBnode $sourceCell $destCell $sourceClockRegion $destClockRegion $BBused [get_BB_clk_regions $sourceClockRegion $destClockRegion] $constraint $status]
 		}
 		incr i
 		lappend overallStatus $status 
 	}
 	if {$table} {
 		$net_table configure -title "CMT Backbone route usage for nets" 
-		puts [$net_table print]\n
+		puts [$net_table print]
+		if {$CDR_backbone_IBUF_I_overall} {
+			puts " * If CLOCK_DEDICATED_ROUTE constraint was applied to fan-in net of IBUF*, CMT Backbone usage report is for fan-out net of IBUF*."
+		}
+		puts ""
 		catch {$net_table destroy}
+		
 	} 
 	# return overall CMT backbone route status of nets 
 	# All Backbone: 		All nets are routed on CMT backbone
